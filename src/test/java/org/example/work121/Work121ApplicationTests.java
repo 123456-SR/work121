040fc112725b9572c9c4a2e5008a5296e9f43498
123456-SR <3338535089@qq.com>
2026-01-21 15:22:31 +0800

    更改

---
+++ src/test/java/org/example/work121/Work121ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.work121;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work121ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
