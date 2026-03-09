8470023b447666a1653f4d4f9474973fbb949385
Sword-keyboard <861417845@qq.com>
2026-03-09 23:04:50 +0800

    更改

--- test_pdf.ps1
+++
@@ -1,9 +0,0 @@
-$body = @{
-    client = "Test"
-    tester = "Test"
-    testerSignature = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=="
-}
-
-Invoke-WebRequest -Uri "http://localhost:8080/api/pdf/preview" -Method POST -ContentType "application/x-www-form-urlencoded" -Body $body -OutFile "test.pdf"
-
-Write-Host "PDF preview test completed. Check test.pdf file."
